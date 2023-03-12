485f868c95bc92c917ef26057cac8930b0701c4c
Julia <jz3942430@gmail.com>
2023-03-12 14:15:00 +0300

    feat: correct styles (Sun, March 12, 2023 2:14:24 PM)

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const accordeons = document.querySelector(".section__accordeon");
 
 const fn = () => {
-  const changeClass = accordeons.map((item) => {
-    item.classList.remove("section__plus");
-    item.classList.add("section__cross");
-    const images = document.querySelectorAll("#section__image");
-    const changeImageClass = images.map((item) => {
-      item.classList.remove("section__image-close");
-      item.classList.add("section__image");
-    });
+  const showImage = accordeons.map((item) => {
+    if (item.matches(":hover")) {
+      const images = document.querySelectorAll("#section__image");
+      const changeImageClass = images.map((item) => {
+        item.classList.remove("section__image-close");
+        item.classList.add("section__image");
+      });
+    }
   });
 };
 
-const addEvent = () => {
-  const accordeon = accordeons.map((item) => {
-    item.addEventListener("click", fn);
-    return item;
-  });
-};
+// const fn = () => {
+//   const changeClass = accordeons.map((item) => {
+//     item.classList.remove("section__plus");
+//     item.classList.add("section__cross");
+//     const images = document.querySelectorAll("#section__image");
+//     const changeImageClass = images.map((item) => {
+//       item.classList.remove("section__image-close");
+//       item.classList.add("section__image");
+//     });
+//   });
+// };
+
+// const addEvent = () => {
+//   const accordeon = accordeons.map((item) => {
+//     item.addEventListener("mouseover", fn);
+//     return item;
+//   });
+// };
